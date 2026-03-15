c706d8b0ba77beb6574dc6f76049fcd10422c8db
GalicaAnna <anjagalica@gmail.com>
2026-03-15 09:54:18 +0200

    CPP test

---
+++ 1.for/Source.cpp
@@ -0,0 +1,10 @@
+# include "iostream"
+#include "Windows.h"
+int main()
+{
+	using namespace std;
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+	cout << "Приввіт гарний і чудових настрій!";
+	return 0;
+}
